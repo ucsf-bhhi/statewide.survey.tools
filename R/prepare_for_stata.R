8142adb63c688a2c4778ab7f1459cc9d23b4b448
Eve Perry <eve.perry@ucsf.edu>
2022-04-01 15:22:42 -0700

    rename known long variable names

--- R/prepare_for_stata.R
+++ R/prepare_for_stata.R
@@ -1,5 +1,19 @@
 prepare_for_stata = function(data) {
-  dplyr::rename_with(data, stata_safe_variable_names, everything())
+  data %>%
+    dplyr::rename(
+      precipitants_to_home_v_0 = precipitants_to_homelessness_timestamp,
+      precipitants_to_home_v_1 = precipitants_to_homelessness_complete,
+      lumpsumsubsidy_preve_v_2 = lumpsumsubsidy_prevention_timestamp,
+      lumpsumsubsidy_preve_v_3 = lumpsumsubsidy_prevention_complete,
+      stable_housing_suppl_v_4 = stable_housing_supplement_timestamp,
+      stable_housing_suppl_v_5 = stable_housing_supplement_complete,
+      history_of_homelessn_v_6 = history_of_homelessness_timestamp,
+      history_of_homelessn_v_7 = history_of_homelessness_complete,
+      income_employment_an_v_8 = income_employment_and_benefits_timestamp,
+      income_employment_an_v_9 = income_employment_and_benefits_complete,
+      healthcare_utilizati_v_10 = healthcare_utilization_timestamp
+    ) %>%
+    dplyr::rename_with(stata_safe_variable_names, everything())
 }
 
 stata_safe_variable_names = function(name) {
